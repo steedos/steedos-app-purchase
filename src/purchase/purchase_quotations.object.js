510f080305499d26fea15166f1192ee01570b801
marchtian <tianqi@hotoa.com>
2020-05-25 15:42:17 +0800

    报价单按钮

--- src/purchase/purchase_quotations.object.js
+++ src/purchase/purchase_quotations.object.js
@@ -1,34 +1,10 @@
 Creator.Objects.purchase_quotations.actions = {
-  printRFQ: {
-      label: "打印询价单",
-      visible: true,
-      on: "record",
-      todo: function (object_name, record_id, fields) {
-        toastr.success("打印采购询价单，待开发");
-      },
-  },
-  emailRFQ: {
-      label: "发送邮件",
-      visible: true,
-      on: "record",
-      todo: function (object_name, record_id, fields) {
-        toastr.success("将采购询价单发送供应商邮箱，待开发");
-      },
-  },
   openQuotation: {
       label: "开启自助报价",
       visible: true,
       on: "record",
       todo: function (object_name, record_id, fields) {
         toastr.success("开启供应商自助报价开关：修改状态，待开发");
-      },
-  },
-  printPQ: {
-      label: "打印报价单",
-      visible: true,
-      on: "record",
-      todo: function (object_name, record_id, fields) {
-        toastr.success("打印采购报价单，待开发");
       },
   },
   confirmQuotation: {
